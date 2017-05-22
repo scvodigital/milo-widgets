4b3b512033c8a305c5cbc0ac10fbbd3ef2ea44fa
tonicblue <tonicblue@gmail.com>
2017-05-22 15:18:19 +0100

    Added Postcode search to Organisations

--- src/js/organisations-widget.ts
+++ src/js/organisations-widget.ts
@@ -63,6 +63,8 @@ class OrganisationWidget extends BaseWidget {
     doSearch(page: number = 1) {
         var query = jq('#mw-organisations-query').val();
         var activity = jq('#mw-organisations-activity').val();
+        var distance = jq('#mw-organisations-distance').val();
+        var postcode = jq('#mw-organisations-user-postcode').val();
 
         var must = [];
 
@@ -83,14 +85,50 @@ class OrganisationWidget extends BaseWidget {
             });
         }
 
-        var payload = {
+        var payload: any = {
             bool: {
                 must: must,
                 minimum_should_match: 1
             }
         };
 
-        this.search(payload, page).then((resultSet: ResultSet) => { this.placeMarkers(resultSet); });
+        if (distance && distance > 0 && postcode) {
+            postcode = postcode.toLowerCase().replace(/[^0-9a-z]/gi, '');
+            jq.getJSON('http://api.postcodes.io/postcodes/' + postcode, (result) => {
+                if (result.status === 200) {
+                    var geo = {
+                        geo_distance_range: {
+                            lt: distance + 'mi',
+                            field: 'geo_coords',
+                            geo_coords: {
+                                lat: result.result.latitude,
+                                lon: result.result.longitude
+                            }
+                        }
+                    }
+
+                    payload.bool.must.push(geo);
+
+                    var sort = [
+                        {
+                            _geo_distance: {
+                                geo_coords: {
+                                    lat: result.result.latitude,
+                                    lon: result.result.longitude
+                                },
+                                order: 'asc',
+                                unit: 'mi',
+                                distance_type: 'arc'
+                            }
+                        }
+                    ];
+                    payload.sort = sort;
+                }
+                this.search(payload, page).then((resultSet: ResultSet) => { this.placeMarkers(resultSet); });
+            });
+        } else {
+            this.search(payload, page).then((resultSet: ResultSet) => { this.placeMarkers(resultSet); });
+        }
     }
 
     placeMarkers(resultSet: ResultSet) {
