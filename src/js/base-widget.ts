8031081c2dc733273f7030bcf410af5890bed9f7
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-07-31 14:59:38 +0100

    Mobile styling improvements

--- src/js/base-widget.ts
+++ src/js/base-widget.ts
@@ -539,9 +539,7 @@ export class BaseWidget {
 
 		if (jump) {
 			var topElement = this.widgetElement.find('[name="mw-' + this.config.name + '-top"]');
-            console.log(topElement);
 			var top = topElement.offset().top;
-            console.log(top);
 			window.scroll(0, top);
             jump = false;
 		}
