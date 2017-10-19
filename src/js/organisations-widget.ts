7ecbd5be8a077f751e208653adc0df4735be8879
Calum MacUisdean <calum.macuisdean@gmail.com>
2017-10-19 12:17:32 +0100

    Scrolling map for enhanced org widget

--- src/js/organisations-widget.ts
+++ src/js/organisations-widget.ts
@@ -15,6 +15,21 @@ class OrganisationWidget extends BaseWidget {
         this.style = this.scriptTag.data('widget-style');
         this.tsi = this.scriptTag.data('tsi');
         this.strive = this.scriptTag.data('strive');
+
+        if (this.style == 'enhanced') {
+            $("#mw-organisations-search-button").click(function() {
+                var $map = $("#mw-organisations-map");
+                var startPosition = $map[0].offsetTop;
+                $(window).scroll(function(){
+                    var newPosition = $(window).scrollTop() - startPosition + 15;
+                    if (newPosition > 0) {
+                        $map.stop().animate({"marginTop": newPosition + "px"}, "slow");
+                    } else {
+                        $map.stop().animate({"marginTop": "0px"}, "slow");
+                    }
+                });
+            });
+        }
     }
 }
 
